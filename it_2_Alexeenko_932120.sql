6559da98de224042389478404840ac33441a1f93
kvintes <vnm111195.ru@gmail.com>
2023-12-22 10:12:58 +0700

    6.4 complete

--- it_2_Alexeenko_932120.sql
+++ it_2_Alexeenko_932120.sql
@@ -2,7 +2,7 @@
 -- Названия функций: F_<имя>. Формат названий процедур: P_<имя>. Написать анонимные блоки или запросы для проверки работы процедур и
 -- функций.
 
-SET search_path TO it;
+SET search_path TO it_2;
 -- 6.1. Написать функцию, которая возвращает количество восхождений для заданного альпиниста в указанный период (id_альпиниста и промежуток
 -- времени – аргументы функции). Если промежуток времени не указан, считается количество за всё время.
 
@@ -220,10 +220,10 @@ LANGUAGE plpgsql
 AS $$ 
 begin
     RAISE NOTICE '%', 
-    (select concat('Календарь восхождений для ',(select ФИО from Альпинисты where id_Альпиниста = p_id_альпиниста) ,' на ' ,year ,' год: ' ,STRING_AGG(total_mrak.mrak, E'\t'))
+    (select concat('Календарь восхождений для ',(select ФИО from Альпинисты where id_Альпиниста = p_id_альпиниста) ,' на ' ,year ,' год: ', E'\n' ,STRING_AGG(total_mrak.mrak, ''), E'\n')
     from
     (
-	    select concat(mrak_sub.s_month, ': ', E'\t' , mrak_sub.s_text) as mrak
+	    select concat(mrak_sub.s_month, ': ', E'\t' , mrak_sub.s_text, E'\n') as mrak
 	    from 
 	    (
 	    	select
@@ -254,12 +254,42 @@ CALL P_get_Climbing_calendar(7, 2023);
 ---ЗАКОНЧИЛИ---
 
 
-
-
-
-
 -- 6.4. Написать процедуру, которая выполняете копирование всех данных об указанном альпинисте, включая восхождения. Аргумент процедуры -
 -- id_альпиниста. Для скопированной записи ставится отметка “копия” в поле ФИО.
+drop PROCEDURE if exists P_copy_info_Альпинист; -- через процедуру
+CREATE OR replace PROCEDURE P_copy_info_Альпинист(
+    p_id_альпиниста integer
+) 
+AS $$ 
+begin
+	INSERT INTO Альпинисты (ФИО, Адрес, Телефон, Дата_рождения)
+  	select 
+        Альпинисты.ФИО || ' (копия)', Альпинисты.Адрес
+        , Альпинисты.Телефон, Альпинисты.Дата_рождения
+    from Альпинисты
+    where id_Альпиниста = p_id_альпиниста
+    ;
+    INSERT INTO Восхождения (Дата_начала, Дата_завершения, ID_Вершины)
+    select 
+        Восхождения.Дата_начала, Восхождения.Дата_завершения, Восхождения.ID_Вершины
+    from Альпинист_Восхождение
+    inner join Восхождения on Восхождения.ID_Восхождения = Альпинист_Восхождение.ID_Восхождения
+    where Альпинист_Восхождение.ID_Альпиниста = p_id_альпиниста
+    ;
+end;
+$$ LANGUAGE plpgsql;
+
+CALL P_copy_info_Альпинист(7);
+
+
+
+select * from Восхождения;
+DELETE FROM Восхождения *
+where Восхождения.ID_Восхождения not in 
+(select distinct on (Дата_начала, Дата_завершения, ID_Вершины, Дней_восхождения) Восхождения.ID_Восхождения from Восхождения)
+;
+select * from Восхождения;
+
 -- 6.5. Написать один или несколько сценариев (анонимных блока) демонстрирующий работу процедур и функций из п. 1-4.
 -- Требование:
 -- - Включение в запрос (для функций)
@@ -268,165 +298,165 @@ CALL P_get_Climbing_calendar(7, 2023);
 
 
 
---------------------------
-select (date_trunc('year', current_timestamp) + interval '1 month'*2 - interval '1 day')::date
-;
+-- --------------------------
+-- select (date_trunc('year', current_timestamp) + interval '1 month'*2 - interval '1 day')::date
+-- ;
 
-    if f_ID_Альпиниста != -1
-        select COALESCE(sub_avg, 0) into avg_durationClimbings
-        from
-        (
-            select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
-            from Вершины 
-            inner join Восхождения on Восхождения.ID_Вершины = Вершины.ID_Вершины
-            inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
-            where 
-                Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
-                and Альпинист_Восхождение.ID_Альпиниста = f_ID_Альпиниста
-        ) as sub
-        ;
-    else
-        select COALESCE(sub_avg, 0) into avg_durationClimbings
-        from
-        (
-            select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
-            from Вершины 
-            inner join Восхождения on Восхождения.ID_Вершины = Вершины.ID_Вершины
-            inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
-            where 
-                Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
-        ) as sub
-    ; 
-    end if;
-    return avg_durationClimbings;
-end;
-$$ language plpgsql;
-
-
+--     if f_ID_Альпиниста != -1
+--         select COALESCE(sub_avg, 0) into avg_durationClimbings
+--         from
+--         (
+--             select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
+--             from Вершины 
+--             inner join Восхождения on Восхождения.ID_Вершины = Вершины.ID_Вершины
+--             inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
+--             where 
+--                 Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
+--                 and Альпинист_Восхождение.ID_Альпиниста = f_ID_Альпиниста
+--         ) as sub
+--         ;
+--     else
+--         select COALESCE(sub_avg, 0) into avg_durationClimbings
+--         from
+--         (
+--             select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
+--             from Вершины 
+--             inner join Восхождения on Восхождения.ID_Вершины = Вершины.ID_Вершины
+--             inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
+--             where 
+--                 Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
+--         ) as sub
+--     ; 
+--     end if;
+--     return avg_durationClimbings;
+-- end;
+-- $$ language plpgsql;
 
 
 
------------------------------------
 
-drop function if exists F_avg_durationClimbings;
-create or replace function F_avg_durationClimbings(
-    f_ID_Вершины integer
-    , f_ID_Альпиниста integer default -1
-    , f_data date default now()::date
-    , f_N_days integer default 100000
-) returns integer as $$
-declare
-    avg_durationClimbings integer := 0
-;
-begin
-    if f_ID_Альпиниста != -1
-        select COALESCE(sub_avg, 0) into avg_durationClimbings
-        from
-        (
-            select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
-            from Вершины 
-            inner join Восхождения on Восхождения.ID_Вершины = Вершины.ID_Вершины
-            inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
-            where 
-                Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
-                and Альпинист_Восхождение.ID_Альпиниста = f_ID_Альпиниста
-        ) as sub
-        ;
-    else
-        select COALESCE(sub_avg, 0) into avg_durationClimbings
-        from
-        (
-            select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
-            from Вершины 
-            inner join Восхождения on Восхождения.ID_Вершины = Вершины.ID_Вершины
-            inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
-            where 
-                Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
-        ) as sub
-    ; 
-    end if;
-    return avg_durationClimbings;
-end;
-$$ language plpgsql;
 
---анонимный запрос для проверки
--- DO $$
--- BEGIN
-    SELECT 
-        Альпинисты.ID_Альпиниста as id,
-        F_countClimbings_climber(Альпинисты.ID_Альпиниста) as count_all_climbings,
-        F_countClimbings_climber(Альпинисты.ID_Альпиниста, now()::date, 30) as count_30days_climbings
-    FROM Альпинисты;
------------------------
---разобраться с функцией и ее тестированием
-drop function if exists F_avg_durationClimbings;
-create or replace function F_avg_durationClimbings(
-    f_ID_Вершины integer
-    , f_ID_Альпиниста integer default -1
-    , f_data date default now()::date
-    , f_N_days integer default 100000
-) returns integer as $$
-declare
-    avg_durationClimbings integer := 0
-;
-begin
-    if f_ID_Альпиниста != -1
-	then
-        select COALESCE(sub_avg, 0) into avg_durationClimbings
-        from
-        (
-            select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
-            from Вершины 
-            inner join Восхождения on Восхождения.ID_Вершины = f_ID_Вершины
-            inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
-            where 
-                Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
-                and Альпинист_Восхождение.ID_Альпиниста = f_ID_Альпиниста
-        ) as sub
-        ;
-    else
-        select COALESCE(sub_avg, 0) into avg_durationClimbings
-        from
-        (
-            select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
-            from Вершины 
-            inner join Восхождения on Восхождения.ID_Вершины = f_ID_Вершины
-            inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
-            where 
-                Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
-        ) as sub
-    ; 
-    end if;
-    return avg_durationClimbings;
-end;
-$$ language plpgsql;
-SELECT 
-        Альпинисты.ID_Альпиниста as id,
-        F_avg_durationClimbings(Альпинисты.ID_Альпиниста, 1, now()::date, 200) as count_all_climbings
-        --F_avg_durationClimbings(Альпинисты.ID_Альпиниста, Альпинисты.ID_Альпиниста, now()::date, 10) as count_15days_climbings
-    FROM Альпинисты;
+-- -----------------------------------
 
-select * 
-from Восхождения
-inner join Альпинист_Восхождение 
-where ID_Альпиниста = 0;
--- END;
--- $$LANGUAGE plpgsql;
--- 6.3. Написать процедуру, которая формирует календарь восхождений для заданного альпиниста. (id_альпиниста и год – параметры функции).
--- Формат вывода:
--- ------------------------------------------------------
--- Календарь восхождений для <ФИО> на <год> год:
--- <месяц_1>:
---  1. <Вершина_1>, <дата начала восхождения> – <дата окончания восхождения>;
---  2. <Вершина_2>, <дата начала восхождения> – <дата окончания восхождения>;
---  <и т. д.> ….
--- <месяц_2 (без восхождений)>: восхождения не запланировано
--- <и т. д.> ….
--- ------------------------------------------------------
--- 6.4. Написать процедуру, которая выполняете копирование всех данных об указанном альпинисте, включая восхождения. Аргумент процедуры -
--- id_альпиниста. Для скопированной записи ставится отметка “копия” в поле ФИО.
--- 6.5. Написать один или несколько сценариев (анонимных блока) демонстрирующий работу процедур и функций из п. 1-4.
--- Требование:
--- - Включение в запрос (для функций)
--- - Для каждой процедуры не менее 3-х примеров работы с различными значениями аргументов.
--- - Комментарии для каждого сценария, описывающие суть примера и результат.
+-- drop function if exists F_avg_durationClimbings;
+-- create or replace function F_avg_durationClimbings(
+--     f_ID_Вершины integer
+--     , f_ID_Альпиниста integer default -1
+--     , f_data date default now()::date
+--     , f_N_days integer default 100000
+-- ) returns integer as $$
+-- declare
+--     avg_durationClimbings integer := 0
+-- ;
+-- begin
+--     if f_ID_Альпиниста != -1
+--         select COALESCE(sub_avg, 0) into avg_durationClimbings
+--         from
+--         (
+--             select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
+--             from Вершины 
+--             inner join Восхождения on Восхождения.ID_Вершины = Вершины.ID_Вершины
+--             inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
+--             where 
+--                 Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
+--                 and Альпинист_Восхождение.ID_Альпиниста = f_ID_Альпиниста
+--         ) as sub
+--         ;
+--     else
+--         select COALESCE(sub_avg, 0) into avg_durationClimbings
+--         from
+--         (
+--             select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
+--             from Вершины 
+--             inner join Восхождения on Восхождения.ID_Вершины = Вершины.ID_Вершины
+--             inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
+--             where 
+--                 Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
+--         ) as sub
+--     ; 
+--     end if;
+--     return avg_durationClimbings;
+-- end;
+-- $$ language plpgsql;
+
+-- --анонимный запрос для проверки
+-- -- DO $$
+-- -- BEGIN
+--     SELECT 
+--         Альпинисты.ID_Альпиниста as id,
+--         F_countClimbings_climber(Альпинисты.ID_Альпиниста) as count_all_climbings,
+--         F_countClimbings_climber(Альпинисты.ID_Альпиниста, now()::date, 30) as count_30days_climbings
+--     FROM Альпинисты;
+-- -----------------------
+-- --разобраться с функцией и ее тестированием
+-- drop function if exists F_avg_durationClimbings;
+-- create or replace function F_avg_durationClimbings(
+--     f_ID_Вершины integer
+--     , f_ID_Альпиниста integer default -1
+--     , f_data date default now()::date
+--     , f_N_days integer default 100000
+-- ) returns integer as $$
+-- declare
+--     avg_durationClimbings integer := 0
+-- ;
+-- begin
+--     if f_ID_Альпиниста != -1
+-- 	then
+--         select COALESCE(sub_avg, 0) into avg_durationClimbings
+--         from
+--         (
+--             select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
+--             from Вершины 
+--             inner join Восхождения on Восхождения.ID_Вершины = f_ID_Вершины
+--             inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
+--             where 
+--                 Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
+--                 and Альпинист_Восхождение.ID_Альпиниста = f_ID_Альпиниста
+--         ) as sub
+--         ;
+--     else
+--         select COALESCE(sub_avg, 0) into avg_durationClimbings
+--         from
+--         (
+--             select avg(COALESCE(Восхождения.Дата_завершения::date, f_data::date) - Восхождения.Дата_начала::date) as sub_avg
+--             from Вершины 
+--             inner join Восхождения on Восхождения.ID_Вершины = f_ID_Вершины
+--             inner join Альпинист_Восхождение on Альпинист_Восхождение.ID_Восхождения = Восхождения.ID_Восхождения
+--             where 
+--                 Восхождения.Дата_начала::date >= f_data - interval '1 days' * f_N_days
+--         ) as sub
+--     ; 
+--     end if;
+--     return avg_durationClimbings;
+-- end;
+-- $$ language plpgsql;
+-- SELECT 
+--         Альпинисты.ID_Альпиниста as id,
+--         F_avg_durationClimbings(Альпинисты.ID_Альпиниста, 1, now()::date, 200) as count_all_climbings
+--         --F_avg_durationClimbings(Альпинисты.ID_Альпиниста, Альпинисты.ID_Альпиниста, now()::date, 10) as count_15days_climbings
+--     FROM Альпинисты;
+
+-- select * 
+-- from Восхождения
+-- inner join Альпинист_Восхождение 
+-- where ID_Альпиниста = 0;
+-- -- END;
+-- -- $$LANGUAGE plpgsql;
+-- -- 6.3. Написать процедуру, которая формирует календарь восхождений для заданного альпиниста. (id_альпиниста и год – параметры функции).
+-- -- Формат вывода:
+-- -- ------------------------------------------------------
+-- -- Календарь восхождений для <ФИО> на <год> год:
+-- -- <месяц_1>:
+-- --  1. <Вершина_1>, <дата начала восхождения> – <дата окончания восхождения>;
+-- --  2. <Вершина_2>, <дата начала восхождения> – <дата окончания восхождения>;
+-- --  <и т. д.> ….
+-- -- <месяц_2 (без восхождений)>: восхождения не запланировано
+-- -- <и т. д.> ….
+-- -- ------------------------------------------------------
+-- -- 6.4. Написать процедуру, которая выполняете копирование всех данных об указанном альпинисте, включая восхождения. Аргумент процедуры -
+-- -- id_альпиниста. Для скопированной записи ставится отметка “копия” в поле ФИО.
+-- -- 6.5. Написать один или несколько сценариев (анонимных блока) демонстрирующий работу процедур и функций из п. 1-4.
+-- -- Требование:
+-- -- - Включение в запрос (для функций)
+-- -- - Для каждой процедуры не менее 3-х примеров работы с различными значениями аргументов.
+-- -- - Комментарии для каждого сценария, описывающие суть примера и результат.
